eab7a4bd1280d1a3af3d2563bf4c98a37599e083
gary-archer <garyarcher36@gmail.com>
2024-09-12 17:33:17 +0100

    Updates

--- start.sh
+++ start.sh
@@ -2,8 +2,13 @@
 
 ##########################################################################
 # A script to build the desktop app and run it
+#
 # On Windows, ensure that you have first set Git bash as the node.js shell
-# npm config set script-shell "C:\\Program Files\\git\\bin\\bash.exe"
+# - npm config set script-shell "C:\\Program Files\\git\\bin\\bash.exe"
+#
+# On Ubuntu 24 you may also need to run the command from this thread:
+# - https://github.com/electron/electron/issues/42510
+# - sudo sysctl -w kernel.apparmor_restrict_unprivileged_userns=0
 ##########################################################################
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
@@ -35,15 +40,6 @@ if [ ! -d 'node_modules' ]; then
   if [ $? -ne 0 ]; then
     echo 'Problem encountered downloading dependencies'
     exit
-  fi
-
-  #
-  # On my Ubuntu 24 system I also need to run these commands
-  # https://github.com/electron/electron/issues/17972
-  #
-  if [ "$PLATFORM" == 'LINUX' ]; then
-    sudo chown root node_modules/electron/dist/chrome-sandbox
-    sudo chmod 4755 node_modules/electron/dist/chrome-sandbox
   fi
 fi
 
